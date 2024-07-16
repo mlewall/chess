c7adb0db263048ecb0b4d80b098c53d4d539d47c
Emily Wall <3milywa11@gmail.com>
2024-07-15 22:24:45 -0600

    Made some investigations into how to make the loginHandler. Was able to extract the login info from the webpage and get it into UserData record. Next: getting that to the service to actually do stuff.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,16 @@
 package service;
 
+import model.UserData;
+import model.AuthData;
+
 public class UserService {
+    public AuthData register(UserData user) {
+        return null;
+    }
+    public static AuthData login(UserData user) {
+        return null;
+    }
+    public void logout(UserData user) {
+    }
+
 }
